59789a0aa6ee0c5e0725d14b6e86bae32edc39a7
Sachin Saini <iamsainisachin@gmail.com>
2020-06-10 01:47:00 +0530

    add initialization function

--- main.go
+++ main.go
@@ -1,35 +1,66 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
+)
+
+const (
+	dotfmDir = ".dotfm"
 )
 
 // vcsCmd describes how to use a version control system
 type vcsCmd struct {
 	name      string
-	cmd       string   // name of binary
-	createCmd []string // command to download a fresh copy of the repository
-	syncCmd   []string // command to sync local and remote repository
-	pingCmd   string   // command to check if a vcs is installed
+	cmd       string // name of binary
+	createCmd string // command to create a new repository
+	cloneCmd  string // command to clone an already existing repository
+	syncCmd   string // command to sync local and remote repository
+	pingCmd   string // command to check if a vcs is installed
+}
+
+// vcsPath describes how to convert a directory path to vcs and repository
+type vcsPath struct {
 }
 
 var vcsGit = &vcsCmd{
 	name:      "Git",
 	cmd:       "git",
-	createCmd: []string{"clone {repo}"},
-	syncCmd:   []string{"pull --ff-only"},
+	createCmd: "init",
+	cloneCmd:  "clone {repo} {dir}",
+	syncCmd:   "pull --ff-only origin master",
 	pingCmd:   "--version",
 }
 
+// checks if git is installed
 func ping(vcsGit *vcsCmd) {
 	cmd := exec.Command(vcsGit.cmd, vcsGit.pingCmd)
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+}
+func initialize(vcsGit *vcsCmd) {
+	// check if dotfmDir directory exists
+	path := filepath.Join(os.Getenv("HOME"), dotfmDir)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, 0755)
+		if err != nil {
+			panic(err)
+		}
+		// initialize git repository
+		cmd := exec.Command(vcsGit.cmd, vcsGit.createCmd, path)
+		err = cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
 	}
 }
 
 func main() {
 	ping(vcsGit)
+	initialize(vcsGit)
 }
